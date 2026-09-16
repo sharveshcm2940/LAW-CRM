060aac69b1f2a4de6e9c9b605613f67f7d2779fe
Sarvxsh555 <sarveshvaranarun@gmail.com>
2026-09-16 16:56:51 +0530

    Error fixes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_URL || '/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -31,7 +33,7 @@ api.interceptors.response.use(
 
       if (refreshToken) {
         try {
-          const res = await axios.post('/api/auth/refresh', { refreshToken });
+          const res = await axios.post(`${BASE_URL}/auth/refresh`, { refreshToken });
           const { accessToken, refreshToken: newRefreshToken } = res.data;
 
           localStorage.setItem('accessToken', accessToken);
